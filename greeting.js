38a68121c2a9c68b44f6d16e21b342ba4748b64f
Minju20200305 <61836008+Minju20200305@users.noreply.github.com>
2021-02-09 15:33:53 +0900

    정리한거

--- greeting.js
+++ greeting.js
@@ -2,7 +2,7 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input");
     greeting = document.querySelector(".js-greetings");
 
-    const USER_LS = "currentUSer";
+    const USER_LS = "currentUser";
         SHOWING_CN = "showing";
 
 function saveName(text){
